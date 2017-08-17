cdc487d259211eafb38d770c7be040dcc902959d
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-08-17 15:43:25 -0700

    Learning multiple line comments

--- ex2.py
+++ ex2.py
@@ -1,9 +1,11 @@
-# A comment, this is so you can read your program later.
-# Anything after the # is ignored by python.
-
+''' How to make a multiline comment A comment, this is so you can read your program later.
+Anything after the # is ignored by python. Not technicaly a comment.
+this is just a weird quirk, the program still reads this.
+'''
+import time
 print("I could have code like this.") # and the comment after is ignored
 
 # You can also ise a comment to "disable" or commont out code:
-# print("This won't run.")
-
+print("This won't run.")
+time.sleep(3) # three seconds is a long time.
 print("This will run.")
