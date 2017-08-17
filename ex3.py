fbd8f3e23afa4a559587471c585d8520c6cb2e1d
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-08-17 15:43:47 -0700

    Lesson 3, mostly done, so frustrated with it.

---
+++ ex3.py
@@ -0,0 +1,29 @@
+print("I will now count my chickens.")
+# usual order of operations so it actally reads 25 + (30/6)=30
+print("Hens", 25 + 30 / 6)
+# I have no clue that the modus does to this,
+# it doesn't seem to be how many times it goes into the usual operation
+print("Roosters", 100 - 25 * 3 % 4.0)
+# adding a decimal into a number seems to make it 3 sig fig.
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# will this change when adding paranthesis
+# don't know where to start adding? or if it will even run with the extra parenthesis
+print("Is it true that 3 + 2 , 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+''' I don't know line 6 is 91. really don't and i'm trying to figure it out with
+the calcultor but my numbers dont match up. Gonna need to pester josh. 
+'''
