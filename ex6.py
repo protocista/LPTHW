ad2a83a4ccd20e24dddf59a92b8a16db8f1faec4
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-08-24 17:38:39 -0700

    Study drills and video watched

--- ex6.py
+++ ex6.py
@@ -1,22 +1,40 @@
+# starting study drills 1
 types_of_people = 10
+# first variable
 x = f"There are {types_of_people} types of people."
-
+# using a variable for a peice of code
 binary = "binary"
+# another varaible
 do_not = "don't"
+# more variables
 y = f"Those who know {binary} and those who {do_not}."
+# second line of using varaible for a peice of code
 
 print(x)
+# short hand code
 print(y)
+# second short hand code
 
 print(f"I said: {x}")
+# a sting with a varaible that is actually code
 print(f"I also said: '{y}' ")
+# second string that uses varaible code switch
 
 hilarious = False
+# the first hint at boolean
 joke_evaluation = "Isn't that joke so funny?! {}"
+# third line of using variable for code
 
 print(joke_evaluation.format(hilarious))
+# this is a wierd orint statement, or rather a new way of using formating rather
+# than just having the f"string" you can use this, which doesnt make much sense yet
 
 w = "This is the left side of..."
 e = "a string with a right side."
 
 print(w + e)
+# using variables for a long string
+# Study drill 2, lines 4, 10, 13, 15, 18, 20, 25
+# Study drill 3, i'm looking at what is being added and what is already there
+# Study drill 4, looks like it makes it easier to break up such a large line of code
+# dont want to break my code, watched video 
