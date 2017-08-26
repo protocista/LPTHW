5ba1ea2ff4614a2334928134bd15d69d6179acf5
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-08-26 16:23:15 -0700

    Study drills added and watched video

--- ex7.py
+++ ex7.py
@@ -1,7 +1,9 @@
 print("Mary had a little lamb.")
 print("Its fleece was white as {}.".format('snow'))
+# snow is not a varaible it is just the placingment of the word
 print("And everywhere that Mary went.")
 print("." * 10) # what'd that do?
+# made the . repeat 10 times
 
 end1 = "C"
 end2 = "h"
@@ -15,7 +17,12 @@ end9 = "r"
 end10 = "g"
 end11 = "e"
 end12 = "r"
+# these are jsut variables
+
 
 # watch that comma at the end. try removing it to see what happens.
+# gives syntax error, so breaking it
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
+# adding that variables and end=' ' gives a space after end6 variable and doesn't
+# make line 26 print on a new line
