fc448ddd08f06c201d69b681a5307ade9da89196
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-08-30 16:01:24 -0700

    Study Drills and video

--- ex9.py
+++ ex9.py
@@ -2,6 +2,7 @@
 
 days = "Mon Tues Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# the \n is saying new line
 
 print("Here are the days: ", days)
 print("Here are the months: ", months)
@@ -12,3 +13,4 @@ With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 linea if we want, or 5, or 6.
 """)
+# The triple quotes is a lomger version of the # for making notes
