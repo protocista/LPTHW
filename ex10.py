cf796022ad76c1973595e7976bbca2c978d63e9e
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-08-30 22:06:16 -0700

    Study drill 1 and 2 and video

--- ex10.py
+++ ex10.py
@@ -2,14 +2,16 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat Food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
-
+'''
+# using """ or ''' is entirely stylistic, usw what ever you want
 print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
+# study drill 3 I will need to come back to.
+# video watched
