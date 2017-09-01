3c9098485e9a648316540230bc9148e2383631a0
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-09-01 14:11:58 -0700

    For real this time

--- ex12.py
+++ ex12.py
@@ -1,10 +1,13 @@
 ''' Study Drills
 1. the pydoc command in termianl what does it do?
-
+input, file, os, and sys really do not make that much sense to me.
+It looks like pydoc is a sort of documenting system for your python program.
+and there are different ways to go about looking, editing, and displaying the
+program.
 '''
 
 age = input("How old are you? ")
 height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
-
+# did this in lesson 11.
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
