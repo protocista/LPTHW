f140f120dc4fe1ae8b994d9a61d352abd74e853b
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-09-01 12:56:05 -0700

    Study Drills

--- ex11.py
+++ ex11.py
@@ -1,8 +1,18 @@
+''' Study Drills
+1. Go online an find out what Python'a input does.
+it asks for data from the user and stores it as a varaible
+2. Can you find other ways to use it? Try some samples you find.
+ask the prompt in the input.
+line 16
+3. Write another "form" like this to ask some questions.
+line 16
+'''
 print("How old are you?", end=' ')
 age = input()
 print("How tall are you?", end=' ')
 height = input()
 print("How much do you weigh?", end=' ')
 weight = input()
-
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+color = input("What is your favorite color? ")
+# prompt 2 and three
+print(f"So, you're {age} old, {height} tall, {weight} heavy and your favorite color is {color}.")
