5d7b4e160e6ee704cd82c8d8054e8fe31f95703a
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-09-01 14:11:37 -0700

    Notes and Study Drills

--- ex12.py
+++ ex12.py
@@ -1,3 +1,8 @@
+''' Study Drills
+1. the pydoc command in termianl what does it do?
+
+'''
+
 age = input("How old are you? ")
 height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
