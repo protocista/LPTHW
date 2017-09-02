b68fc1d6127dc5d91effd4a7e5196208f0827702
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-09-01 21:41:04 -0700

    Study drills 1 and 2 done

--- ex13.py
+++ ex13.py
@@ -1,8 +1,14 @@
 from sys import argv
 # read the WYSS section for how to run this
-script, first, second, third = argv
-
+script, first, second, third, fourth = argv
+# removed third, then added fourth
 print("The script is called:", script)
 print("The first variable is:", first)
 print("The second variable is:", second)
 print("The third variable is:", third)
+print("the fourth variable is:", fourth)
+
+''' Study Drills
+This is starting to get a bit complicated now.
+I don't understand how to combine input and argv
+'''
