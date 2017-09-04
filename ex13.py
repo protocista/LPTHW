efa16765eed6ff1d4aa2e3c15789e2615e905462
Lydia Tomeo <lydia.tomeo@gmail.com>
2017-09-03 17:13:19 -0700

    Study drills 3 and 4 as well as videos

--- ex13.py
+++ ex13.py
@@ -1,14 +1,18 @@
-from sys import argv
+from sys import argv # argv = argument varaible(vector) feature/module
 # read the WYSS section for how to run this
 script, first, second, third, fourth = argv
 # removed third, then added fourth
+print(">>>> argv=", repr(argv))
+# added from video, repr lets you read the inputs that you assigned, debugging tool
 print("The script is called:", script)
 print("The first variable is:", first)
 print("The second variable is:", second)
 print("The third variable is:", third)
-print("the fourth variable is:", fourth)
-
-''' Study Drills
-This is starting to get a bit complicated now.
-I don't understand how to combine input and argv
-'''
+print("The fourth variable is:", fourth)
+count = input("Are there other variables you can think of? ")
+print(f"""
+The script {script} has multiple variables.
+They are called {first}, {second},
+{third} and {fourth}.
+""")
+# study drill 3 and 4
